e65e4175eff17cb3ebccdbe03e364f06ad0490dc
v-biryukov <biryukov.vova@gmail.com>
2020-11-30 23:32:58 +0300

    Term1: Safety

---
+++ term1/seminar14_safety/classroom_tasks/files/perrors.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+
+int main()
+{
+	perror("Error");
+
+	// Неправильная работа с математическими функциями
+	float x = sqrt(-1);
+	perror("Error");
+
+	// Выделяем больше чем нужно
+	int* p = (int*)malloc(10000000000000);
+	perror("Error");
+
+	// Пытаемся открыть файл, который не существует
+	FILE* f1 = fopen("non_existed_file.txt", "r");
+	perror("Error");
+
+	// Неправильный режим открытия файла (z)
+	FILE* f2 = fopen("non_existed_file.txt", "z");
+	perror("Error");
+}
---
+++ term1/seminar14_safety/classroom_tasks/files/scanf_safety.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+
+int main()
+{
+	char a[5] = "Lion";
+	char b[5];
+
+	scanf("%s", b);
+
+	printf("a = %s\n", a);
+	printf("b = %s\n", b);
+
+	// Что напечатает эта программа, если подать на вход следующие строки:
+	// Cat   Zebra   Elephant
+	// Почему так происходит?
+	// Как это исправить?
+}
